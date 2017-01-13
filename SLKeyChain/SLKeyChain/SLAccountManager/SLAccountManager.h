21b0260278886f09c86c5665baa761d50d37e3a0
LZY <958246321@qq.com>
2017-01-13 13:48:01 +0800

    添加账户管理和账户Model

---
+++ SLKeyChain/SLKeyChain/SLAccountManager/SLAccountManager.h
@@ -0,0 +1,13 @@
+//
+//  SLAccountManager.h
+//  SLKeyChain
+//
+//  Created by lirenqiang on 2017/1/13.
+//  Copyright © 2017年 Ninja. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SLAccountManager : NSObject
+
+@end
---
+++ SLKeyChain/SLKeyChain/SLAccountManager/SLAccountManager.m
@@ -0,0 +1,13 @@
+//
+//  SLAccountManager.m
+//  SLKeyChain
+//
+//  Created by lirenqiang on 2017/1/13.
+//  Copyright © 2017年 Ninja. All rights reserved.
+//
+
+#import "SLAccountManager.h"
+
+@implementation SLAccountManager
+
+@end
